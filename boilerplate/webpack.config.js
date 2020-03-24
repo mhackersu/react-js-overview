67f77cd1ca939aa94f8ad48415a3bec1c2f26338
Mike Hacker <desertsofcacti@gmail.com>
2020-03-24 15:37:09 -0500

    boilerplate housekeeping

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -1,31 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./app/index.js",
+  entry: './app/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
   },
   module: {
     rules: [
-      /*
-       **       These are like "pre-processors"
-       */
-      { test: /\.(js)$/, use: "babel-loader" }, // (js) will select all JS files
-      { test: /\.css$/, use: ["style-loader", "css-loader"] }
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
-  /*
-   **   Development mode
-   */
-  mode: "development",
+  mode: 'development',
   plugins: [
-    /*
-     **     Use the ./app/index.html as a template and create a new index.html while also adding the new index.js bundle
-     */
     new HtmlWebpackPlugin({
-      template: "app/index.html"
+      template: 'app/index.html'
     })
   ]
 };
